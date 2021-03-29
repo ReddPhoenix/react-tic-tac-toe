620595267ea7833fabf412893b25b3757e09faa4
Walter Guerrero <wsguerrero@gmail.com>
2021-03-28 22:14:28 -0600

    created Game component

--- src/index.js
+++ src/index.js
@@ -41,3 +41,22 @@ class Board extends React.Component {
         );
     }
 }
+
+class Game extends React.component {
+    render() {
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board />
+                </div>
+                <div className='game-info'>
+                    <div> </div>
+                    <ol> </ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game />, document.getElementById('root'));
